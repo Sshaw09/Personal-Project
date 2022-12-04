1cc7746ce3486d6c43f1589e8b3ec470c690e871
Sshaw09 <97680673+Sshaw09@users.noreply.github.com>
2022-12-04 14:00:01 -0500

    Player Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float speed = 10f;
+    Rigidbody playerRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRB.AddForce(Vector3.up * speed, ForceMode.Impulse);
+        }
+
+    }
+}
